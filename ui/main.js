e6ba4795b9be232d36d471b6eb41e7eeb55ba02b
gnanajoswin <30952239+gnanajoswin@users.noreply.github.com>
2017-08-13 01:48:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
-var button=document.grtElementById('counter');
+var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function()
 {
     counter=counter+1;
-    var span=document.grtElementById('count');
+    var span=document.getElementById('count');
     span.innerHTML=count.toString();
     
 };
