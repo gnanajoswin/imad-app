2fd088c22acffdc05151a2ac8272ac508e6914d4
gnanajoswin <30952239+gnanajoswin@users.noreply.github.com>
2017-08-13 02:23:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,8 +15,8 @@ button.onclick=function()
     
            }
        }
-     
+     };
 request.open('GET','http://pbjoswin97.imad.hasura-app.io/counter',true);
 request.send(null);
-     };    
+         
 };
