1c7246d3278222bcb1ebb99eae97fbf37fcf6289
gnanajoswin <30952239+gnanajoswin@users.noreply.github.com>
2017-08-13 00:55:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ console.log('Loaded!');
 var element=document.getElementById('main=text'
 );
 element.innerHTML= 'newvalue';
-var img=document.getElementById('img');
+var img=document.getElementById('modi');
 imag.onclick=function(){
   img.style.marginleft='100px' ; 
 };
