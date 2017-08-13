73aee3d0821c6da579c5250aee59c19d3d5881ba
gnanajoswin <30952239+gnanajoswin@users.noreply.github.com>
2017-08-13 14:49:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,14 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=['name1','name2','name3'];
+    var request=new XMLhttpRequest();
+     //response
+     request.onreadystatechange=function(){
+       if(request.readyState===XMLhttpRequest.DONE)  {
+           if(request.status===200)
+           {
+              var names=request.responseText;
+              names=JSON.parse(names);
     var list='';
     for(var i=0;i<name.length;i++){
         list+='<li>'+name[i]+'</li>';
@@ -33,5 +40,14 @@ submit.onclick=function(){
         
     }
     var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+    ul.innerHTML=list;   
+           }
+       }
+     };
+request.open('GET','http://pbjoswin97.imad.hasura-app.io/submit_name?name=+name',true);
+request.send(null);
+         
+
+
+  
 };
