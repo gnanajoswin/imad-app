46a709d79d4cb02bcfedabf4ecfd4016d2db0e32
gnanajoswin <30952239+gnanajoswin@users.noreply.github.com>
2017-08-15 00:52:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
+var config={
+    user:'pbjoswin97',
+    database: 'pbjoswin97',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -89,6 +97,17 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool=new Pool(config);
+app.get('/test.db',function(req,res){
+    pool.query('SELECT*FROM test',function(err,result){
+        if(err){
+            res.result(500).send(err.toString())
+        }else{
+            res.send(JSON.stringfy(result));
+        }
+        
+    });
 });
 var counter=0;
 app.get('/counter',function(req,res){
